5e26e5e296322b11be6907c738ab87872c249277
Xavier Simó <xsimov@gmail.com>
2015-04-17 17:59:46 +0200

    First version
    Created during the Martian Day 17/04/2015

---
+++ install.sh
@@ -0,0 +1,28 @@
+# Install zsh shell with oh-my-zsh
+curl -L github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+git clone http://github.com/MarsBased/dev-config /tmp/marstemplate
+
+# Copy configuration for zsh
+mv ~/.zshrc ~/.zshrc.base
+cp /tmp/marstemplate/zsh/.zshrc ~/.zshrc
+
+# Copy .gitignore
+mv ~/.gitignore ~/.gitignore.old
+cp /tmp/marstemplate/zsh/.gitignore ~/.gitignore
+
+# Install Package Control for Sublime
+wget https://packagecontrol.io/Package%20Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+
+# Copy sublime snippets
+cp /tmp/marstemplate/sublime/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install Caskroom/cask/java
+
+brew install rbenv ruby-build mysql postgresql elasticsearch nodejs imagemagick mongodb redis
+
+brew update && brew upgrade ruby-build
+
+rbenv install 2.2.2
